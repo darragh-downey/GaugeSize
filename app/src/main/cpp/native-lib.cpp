d38a1c72b33b0d0736f05d580813eb19fc961953
Darragh Downey <darragh.downey90@gmail.com>
2017-07-18 17:31:02 +1000

    updated native-lib

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,40 +4,10 @@
 #include "detect_reference_obj.h"
 
 extern "C" {
-JNIEXPORT jstring JNICALL
-        Java_com_ddowney_gaugesize_MainActivity_stringFromJNI(JNIEnv *env,
-                                                              jobject /* this */);
 
-JNIEXPORT jint JNICALL
-        Java_com_ddowney_gaugesize_MainActivity_intFromJNI(JNIEnv *env,
-                                                           jobject /* this */);
-
-JNIEXPORT jstring JNICALL
-        Java_com_ddowney_gaugesize_MainActivity_stringFromOtherCall(JNIEnv *env,
-                                                                    jobject /* this */);
+JNIEXPORT void JNICALL
+        Java_com_ddowney_gaugesize_Cam2BasicFragment_processImage(JNIEnv *env, jobject);
 };
-
-JNIEXPORT jstring JNICALL
-Java_com_ddowney_gaugesize_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "This string came from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
-JNIEXPORT jint JNICALL
-Java_com_ddowney_gaugesize_MainActivity_intFromJNI(
-        JNIEnv *env,
-        jobject /* this */){
-    return 13;
-}
-
-JNIEXPORT jstring JNICALL
-Java_com_ddowney_gaugesize_MainActivity_stringFromOtherCall(
-        JNIEnv *env,
-        jobject /* this */) {
-    return env->NewStringUTF(extclass::extstring().c_str());
-}
 
 JNIEXPORT void JNICALL
 Java_com_ddowney_gaugesize_Cam2BasicFragment_processImage(
